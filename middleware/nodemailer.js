f4d812fd7c5eea49daa09a1a80c0052ec4a2ee85
chiranjitmu/ <chiranjitmukherjee071@gmail.com>
2023-12-14 18:42:22 +0530

    added with gmail body message

--- middleware/nodemailer.js
+++ middleware/nodemailer.js
@@ -25,8 +25,24 @@ const sendEmail = async (transporter, mailOptions) => {
       const mailOptions = {
         from: "Chitchat.chir@gmail.com",
         to: email,
-        subject: "Your OTP",
-        text: `Your Chit-Chat OTP is ${otp}`,
+        subject: "Your Chit-Chat OTP",
+        text: `Dear ${email},
+
+        Thank you for choosing Chit-Chat! To ensure the security of your 
+        account, we have generated a One-Time Password (OTP) for you.
+        
+        Your OTP: ${otp}
+        
+        Please use this OTP to complete the authentication process. It is valid 
+        for a single use and will expire in [3 min].
+        
+        If you did not request this OTP or have any concerns about your account 
+        security, please contact our support team immediately at [Chitchat.chir@gmail.com].
+        
+        Thank you for using Chit-Chat!
+        
+        Best regards,
+        The Chit-Chat Team`,
       };
   
       await sendEmail(transporter, mailOptions);
