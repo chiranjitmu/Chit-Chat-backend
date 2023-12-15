a71c1b24653dbbe57178abebd60962e1367f08d4
chiranjitmu/ <chiranjitmukherjee071@gmail.com>
2023-12-15 13:11:41 +0530

    final deploy9

--- index.js
+++ index.js
@@ -98,6 +98,6 @@ io.on("connection", (socket) => {
 
 // listener
 app.listen(port1, () => console.log(`listening on ${port1}`));
-server.listen(port2, () => {
-  console.log("Server is running");
-});
+// server.listen(port2, () => {
+//   console.log("Server is running");
+// });
